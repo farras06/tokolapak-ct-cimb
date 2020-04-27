5a72707feb88b8b72a5b40e94567581433e81b61
farras06 <farrasammar06@gmail.com>
2020-04-27 12:47:47 +0700

    saved

--- src/constants/API.js
+++ src/constants/API.js
@@ -1,5 +1,1 @@
-<<<<<<< HEAD
-export const API_URL = "http://localhost:3001"
-=======
 export const API_URL = `http://localhost:3001`;
->>>>>>> 6dba32738aaf227d6a159edcd2d87bf720b06f33
--- src/redux/reducers/user.js
+++ src/redux/reducers/user.js
@@ -1,13 +1,7 @@
-<<<<<<< HEAD
 import userType from "../types/user"
 import { stat } from "fs";
 
 const { ON_LOGIN_SUCCESS, ON_LOGIN_FAIL, ON_LOGOUT_SUCCESS, ON_REGISTER_FAIL } = userType
-=======
-import userTypes from "../types/user";
-
-const { ON_LOGIN_FAIL, ON_LOGIN_SUCCESS, ON_LOGOUT_SUCCESS } = userTypes;
->>>>>>> 6dba32738aaf227d6a159edcd2d87bf720b06f33
 
 const init_state = {
   id: 0,
@@ -19,10 +13,6 @@ const init_state = {
 };
 
 export default (state = init_state, action) => {
-<<<<<<< HEAD
-
-=======
->>>>>>> 6dba32738aaf227d6a159edcd2d87bf720b06f33
   switch (action.type) {
     case ON_LOGIN_SUCCESS:
       const { username, fullName, role, id } = action.payload;
@@ -33,7 +23,6 @@ export default (state = init_state, action) => {
         role,
         id,
       };
-<<<<<<< HEAD
 
     case ON_REGISTER_FAIL:
       return { ...state, errMsg: action.payload }
@@ -45,15 +34,3 @@ export default (state = init_state, action) => {
       return { ...state }
   }
 };
-=======
-    case ON_LOGIN_FAIL:
-      return { ...state, errMsg: action.payload };
-    case "ON_REGISTER_FAIL":
-      return { ...state, errMsg: action.payload };
-    case ON_LOGOUT_SUCCESS:
-      return { ...init_state };
-    default:
-      return { ...state };
-  }
-};
->>>>>>> 6dba32738aaf227d6a159edcd2d87bf720b06f33
