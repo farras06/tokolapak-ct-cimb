940bb45c3f59b1dafec82b21f77d4b946bb80533
farras06 <farrasammar06@gmail.com>
2020-04-27 12:14:50 +0700

    AUth page

--- src/redux/reducers/user.js
+++ src/redux/reducers/user.js
@@ -1,9 +1,36 @@
+import userType from "../types/user"
+import { stat } from "fs";
+
+const { ON_LOGIN_SUCCESS, ON_LOGIN_FAIL, ON_LOGOUT_SUCCESS, ON_REGISTER_FAIL } = userType
+
 const init_state = {
   id: 0,
   username: "",
+  fullName: "",
+  address: {},
   role: "",
 };
 
 export default (state = init_state, action) => {
-  return { ...state };
+
+  switch (action.type) {
+    case ON_LOGIN_SUCCESS:
+      const { username, fullName, role, id } = action.payload;
+      return {
+        ...state,
+        username,
+        fullName,
+        role,
+        id,
+      };
+
+    case ON_REGISTER_FAIL:
+      return { ...state, errMsg: action.payload }
+
+    case ON_LOGIN_FAIL:
+      return { ...state, errMsg: action.payload }
+
+    default:
+      return { ...state }
+  }
 };
